cf7f747b539bb227d083d604dfcfc0163c5c11fa
UtJha <utkarshjhaonline@gmail.com>
2024-07-29 03:06:00 +0530

    setting up authentication app

--- ecomm/urls.py
+++ ecomm/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("ecommerceapp.urls"))
+    path('', include("ecommerceapp.urls")),
+    path('authentication/', include("authenticationapp.urls")),
 ]
